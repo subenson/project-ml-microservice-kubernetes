47d26a67b7a1c889f6eb42f3cd1cc60b65e2f98b
svendenotter <sven.den.otter@rebilly.com>
2022-09-12 09:23:23 +0900

    Deploy docker image

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+docker build -t project-ml .
 
-# Step 1:
-# Build image and add a descriptive tag
+docker tag project-ml svendenotter/project-ml:v1.0.0
 
-# Step 2: 
-# List docker images
+docker images
 
-# Step 3: 
-# Run flask app
+docker run -d -p 80:80 project-ml
+
+docker ps
